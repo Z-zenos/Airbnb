f80b17582604909d081f807845f3ff10b654caed
Z-techIT <tuan.ha194705@sis.hust.edu.vn>
2023-12-27 06:38:54 +0700

    UI: Fix input

--- client/src/components/Input/Input.jsx
+++ client/src/components/Input/Input.jsx
@@ -1,14 +1,12 @@
-import { forwardRef, useState } from "react";
+import { forwardRef } from "react";
 import "./Input.css";
 
-function CostomInput(props, ref) {
+function CustomInput(props, ref) {
   const { 
     label, className, name, value,
     wrapperStyle, errors, beforeText,
     ...rest
   } = props;
-
-  const [isFocused, setIsFocused] = useState(false);
 
   function handleClick(ev) {
     const wrapper = ev.target;
@@ -21,7 +19,6 @@ function CostomInput(props, ref) {
       className={"input-container relative border border-gray-400 pt-6 pb-2 px-5 focus-within:rounded-md focus-within:outline focus-within:outline-black focus-within:outline-2 focus-within:outline-offset-1 focus-within:border-y-gray-400 flex flex-row justify-start items-center gap-1 " + className} 
       style={wrapperStyle}
       onClick={handleClick}
-      onBlur={() => setIsFocused(false)}
     >
       { ((value || beforeText)) && <span className={"text-gray-500 text-sm -translate-x-[3px] " + (value ? "" : "w-7 mr-2")}>{beforeText}</span> }
       
@@ -32,17 +29,17 @@ function CostomInput(props, ref) {
       <input 
         className="w-full inline h-full focus:border-none focus:outline-none text-[16px] "
         name={name}
-        onFocus={() => setIsFocused(true)}
         ref={ref}
         value={value}
+        aria-invalid={errors?.[name] ? "true" : "false"}
         {...rest}
       >
       </input>
       {label && <label className={value && 'filled'} htmlFor={name}>{label}</label> }
       { (errors?.[name] && value) && <p className="absolute top-[7px] right-3 text-primary text-xs">{errors?.[name]?.message}</p> }
     </div>
-  );
+  )
 }
 
-const Input = forwardRef(CostomInput);
+const Input = forwardRef(CustomInput);
 export default Input;
--- client/src/utils/timeAgo.js
+++ client/src/utils/timeAgo.js
@@ -20,15 +20,15 @@ export default function timeAgo(time) {
     [3600, 'minutes', 60], // 60*60, 60
     [7200, '1 hour ago', '1 hour from now'], // 60*60*2
     [86400, 'hours', 3600], // 60*60*24, 60*60
-    [172800, 'Yesterday', 'Tomorrow'], // 60*60*24*2
+    [172800, 'yesterday', 'tomorrow'], // 60*60*24*2
     [604800, 'days', 86400], // 60*60*24*7, 60*60*24
-    [1209600, 'Last week', 'Next week'], // 60*60*24*7*4*2
+    [1209600, 'last week', 'next week'], // 60*60*24*7*4*2
     [2419200, 'weeks', 604800], // 60*60*24*7*4, 60*60*24*7
-    [4838400, 'Last month', 'Next month'], // 60*60*24*7*4*2
+    [4838400, 'last month', 'next month'], // 60*60*24*7*4*2
     [29030400, 'months', 2419200], // 60*60*24*7*4*12, 60*60*24*7*4
-    [58060800, 'Last year', 'Next year'], // 60*60*24*7*4*12*2
+    [58060800, 'last year', 'next year'], // 60*60*24*7*4*12*2
     [2903040000, 'years', 29030400], // 60*60*24*7*4*12*100, 60*60*24*7*4*12
-    [5806080000, 'Last century', 'Next century'], // 60*60*24*7*4*12*100*2
+    [5806080000, 'last century', 'next century'], // 60*60*24*7*4*12*100*2
     [58060800000, 'centuries', 2903040000] // 60*60*24*7*4*12*100*20, 60*60*24*7*4*12*100
   ];
 
@@ -37,7 +37,7 @@ export default function timeAgo(time) {
     list_choice = 1;
 
   if (seconds == 0) {
-    return 'just now'
+    return 'just now';
   }
 
   if (seconds < 0) {
